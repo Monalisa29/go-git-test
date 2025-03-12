67eabded3c49143ddde72fbf3f48c78c1263c6fa
Monalisa29 <150048736+Monalisa29@users.noreply.github.com>
2025-03-12 14:28:13 +0530

    Update greet_test.go

--- greet_test.go
+++ greet_test.go
@@ -1,13 +1,15 @@
 package main
- 
+
 import "testing"
- 
+
 // TestGreet tests the Greet function
 func TestGreet(t *testing.T) {
     result := Greet("Git")
     expected := "Hello, Git!"
-    
+
+    // If the result is not equal to the expected value, the test fails
     if result != expected {
         t.Errorf("Greet() = %v; want %v", result, expected)
+
     }
 }
